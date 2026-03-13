cd1f15df20a456ccb2521f17f2e3beb45da5368c
rayan-hmd <s4091971@student.rmit.edu.au>
2026-03-13 17:29:00 +1100

    tests draft

---
+++ tests/api.spec.js
@@ -0,0 +1,34 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+const API_URL = 'https://73nhe22wt4.execute-api.ap-southeast-4.amazonaws.com/default/incrementVisitorCount';
+
+test('API returns visitor count', async ({ request }) => {
+    const response = await request.get(API_URL);
+    expect(response.status()).toBe(200);
+    const data = await response.json();
+
+    expect(data).toHaveProperty("visitor_count");
+    expect(typeof data.count).toBe("number");
+
+});
+
+test('Visitor count increments', async ({ request }) => {
+    const response1 = await request.get(API_URL);
+    const first = (await response1.json()).visitor_count;
+
+    const response2 = await request.get(API_URL);
+    const second = (await response2.json()).visitor_count;
+
+    expect(second).toBe(first + 1);
+});
+
+test('API response structure is correct', async ({ request }) => {
+    const response = await request.get(API_URL);
+    expect(response.status()).toBe(200);
+    const data = await response.json();
+    expect(data).toEqual(
+        expect.objectContaining({
+            visitor_count: expect.any(Number)
+        })
+    );
+});
---
+++ tests/e2e.spec.js
@@ -0,0 +1,14 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+const API_URL = 'https://73nhe22wt4.execute-api.ap-southeast-4.amazonaws.com/default/incrementVisitorCount';
+
+test('website displays visitor count', async ({ page }) => {
+    await page.goto('https://www.rayanhameed.com');
+
+    const visitorCount = await page.locator('#visitor-count').textContent();
+    const counter = page.locator("#visitor-count");
+    
+    await expect(counter).toBeVisible();
+    await expect(visitorCount).toMatch(/Visitor Count: \d+/);
+}
+);
